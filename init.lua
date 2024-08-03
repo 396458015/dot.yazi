3534a3c5a0a74f4085e8b07f77d9da5694112684
396458015 <396458015@qq.com>
2024-08-03 17:24:41 +0800

    add searchjump plugin

--- init.lua
+++ init.lua
@@ -27,5 +27,25 @@
 --         Preview:render(chunks[3]:padding(ui.Padding.xy(1))),
 --     }
 -- end
+
+-- plugin
+-- starship.yazi
 require("starship"):setup()
+
+-- searchjump.yazi
+require("searchjump"):setup {
+	unmatch_fg = "#9ca0b0",
+    match_str_fg = "#000000",
+    match_str_bg = "#84c443",
+    first_match_str_fg = "#000000",
+    first_match_str_bg = "#84c443",
+    lable_fg = "#eeeeee",
+    lable_bg = "#b34607",
+    only_current = false, -- only search the current window
+    show_search_in_statusbar = true,
+    auto_exit_when_unmatch = true,
+    enable_capital_lable = false,
+    search_patterns = {}  -- demo:{"%.e%d+","s%d+e%d+"}
+}
+
 
